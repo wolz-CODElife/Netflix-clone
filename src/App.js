75380eb0cbd9576082e0ec206a72ea8b3310b829
wolz-CODElife <joeladewole3@gmail.com>
2021-07-26 16:39:53 +0100

    Intanciating new netlfix app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Index from './components/Index/Index';
-import Create from './components/Create/Create';
-import Error from './components/Error/Error'
-
-
-
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" children={<Index />} />
-        <Route exact path="/create" children={<Create />} /> 
-        <Route path="*"><Error /> </Route>       
-      </Switch>
-    </Router>
+    <div className="App">
+      <h1></h1>
+    </div>
   )
 }
 
-export default App;
+export default App
